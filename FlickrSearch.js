c1d4a1a609ccb30c374539bef98ad168da050bc0
Mattias Svedklint <m.svedklint@telia.com>
2015-02-12 14:32:53 +0100

    Add filter function and resize picture

--- FlickrSearch.js
+++ FlickrSearch.js
@@ -3,48 +3,141 @@
     var searchText = "";
     var url = "";
     var i;
-    var button = document.getElementById("searchButton");
-    button.addEventListener('click', function () {
-        console.log("test");
-        searchText = document.getElementById("searchText").value;
-        console.log(searchText);
-        url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=cdf8b4abca85ccae97559f5c0492a2bb&text=" + searchText + "&format=json&nojsoncallback=1";
-        test();
-    });
+    var viewGallery = false;
+    var searchText = document.getElementById("searchText");
 
-    function test() {
+    //content that going to contain all pictures
+    var content = document.getElementById("content");
+
+    //search button
+    var searchButton = document.getElementById("searchButton");
+    searchButton.addEventListener('click', function () {
+        getSearchText();
+    });
+    
+    //key event for textbox
+    searchText.onkeypress = function(evt) {
+        //if enter
+        if(evt.keyCode === 13){
+            getSearchText();
+        }
+    };
+
+    //filter button to view gallery of choosed pictures
+    var filterButton = document.getElementById("filterButton");
+    filterButton.addEventListener('click', function () {
+        if (viewGallery === false) {
+            var childNodes = content.childNodes;
+            var choosedPics = [];
+            viewGallery = true;
+
+            //get all chosed picture and add them to an array
+            for (i = 0; i < childNodes.length; i++) {
+                if (childNodes[i].style.borderColor !== "") {
+                    choosedPics.push(childNodes[i]);
+                }
+            }
+
+            //clear content
+            while (content.firstChild) {
+                content.removeChild(content.firstChild);
+            }
+
+            //add chosed picture to content
+            for (i = 0; i < choosedPics.length; i++) {
+                {
+                    choosedPics[i].style.borderColor = "";
+                    choosedPics[i].style.borderWidth = "0";
+                    content.appendChild(choosedPics[i]);
+                }
+            }
+        }
+    });
+    
+    /**
+     * Set url and if textbox not empty it calls for function to get photos
+     * 
+     */
+    function getSearchText(){
+        viewGallery = false;      
+        if(searchText !== ""){
+            url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3193d22533c7234cc79736cff4d688e5&text=" + searchText.value + "&format=json&nojsoncallback=1";
+            getPhotos();
+        }
+    }
+
+    /**
+     * get all picture information in a json file
+     * 
+     */
+    function getPhotos() {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                var myArr = JSON.parse(xmlhttp.responseText);
-                print(myArr.photos.photo);
+                var pictureArray = JSON.parse(xmlhttp.responseText);
+                print(pictureArray.photos.photo);
             }
         };
         xmlhttp.open("GET", url, true);
         xmlhttp.send();
     }
+    
 
+    /**
+     * Add all pictures with eventhandler to content.
+     * 
+     * @param {string} arr contain information about pictures
+     */
     function print(arr) {
-        var content = document.getElementById("content");
         content.innerHTML = "";
+
+        //add all picture with for loop
         for (i = 0; i < arr.length; i++) {
+
+            //create new div that contain a picture
             var div = document.createElement('div');
             div.className = "pictureFrame";
             div.id = "picture" + i;
-            div.innerHTML = '<img class="images" src="https://farm' + arr[i].farm + '.staticflickr.com/' + arr[i].server + '/' + arr[i].id + '_' + arr[i].secret + '_m.jpg" alt="' + arr[i].title + '">';
+
+            //add pictrue adress to picture on flickr
+            div.innerHTML = '<img id="image' + i + '" class="images" src="https://farm' + arr[i].farm + '.staticflickr.com/' + arr[i].server + '/' + arr[i].id + '_' + arr[i].secret + '.jpg" alt="' + arr[i].title + '">';
+
+            //add eventlistener to picture 
             div.addEventListener('click', function (value) {
-                if (value.path[1].style.borderColor !== "red") {
-                    value.path[1].style.borderColor = "red";
-                }else{
-                    value.path[1].style.borderColor = "";
+
+                //check if choosed picture is viewed in gallery
+                if (viewGallery === false) {
+                    //change color on frame around picture when click on it
+                    if (value.target.parentNode.style.borderColor === "") {
+                        value.target.parentNode.style.borderColor = "#fb0404";
+                    } else {
+                        value.target.parentNode.style.borderColor = "";
+                    }
+                } else {
+
+                    //set transition when picture change size
+                    value.target.style.transition = "width 1s, height 2s";
+
+                    //resize picture
+                    if (value.target.style.width !== "100%") {
+                        value.target.parentNode.style.width = "40%";
+                        value.target.parentNode.style.height = "40%";
+                        value.target.style.width = "100%";
+                        value.target.style.height = "100%";
+                    } else {
+                        value.target.style.width = "";
+                        value.target.style.height = "";
+                        value.target.parentNode.style.width = "10vw";
+                        value.target.parentNode.style.height = "6vw";
+
+                    }
                 }
-                    
+
             });
+
+            //add picture to content
             content.appendChild(div);
         }
     }
-    
-    //TODO visa endast valda bilder i ett galleri
-    //     klicka på bild så förstoras den
 }());
 
